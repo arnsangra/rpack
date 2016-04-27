cde624133e38f321a6443b8b718f0988a71a2609
vint-devel <vint-devel@ackcent.com>
2016-04-27 11:46:08 +0000

    Fixed order of move command. Still need not implemented buid-packages folder existance check.

--- rpack.sh
+++ rpack.sh
@@ -66,8 +66,8 @@ else
 
         sudo R CMD REMOVE $PCK_NAME
         sudo R CMD build $PCK_PATH
-        mv "`pwd`/$BUILD_FILE" "$REP_LOC/$BUILD_FILE"
         sudo R CMD INSTALL $LIB_PATH $BUILD_FILE
+        mv "`pwd`/$BUILD_FILE" "$HOME/build-packages/$BUILD_FILE"
 
         if [[ $keep -ne 1 ]]; then
             rm -vf $BUILD_FILE
