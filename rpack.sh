89836ddcc57e10fa40a163ccc9107b9493719939
Arnau Sangrà <asangra@ackcent.com>
2016-06-02 11:37:46 +0200

    Improvements for rpack..
    - Support for alternative pacakge location directory through $REP_ALT_LOC env variable.
    - Utilization of random directory to build package.
    - Check existance of build-packages folder before attempting to keep package tar.
    - Improvement of regex pattern to detect new package version.

--- rpack.sh
+++ rpack.sh
@@ -2,7 +2,16 @@
 
 PRGM_NAME="rpack"
 REP_LOC="$HOME/repositories"
+BUILD_PCKGS_DIR="$HOME/build-packages"
 OS=`uname`
+
+MYTMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+
+# deletes the temp directory
+#function cleanup { rm -rf "$MYTMPDIR" echo "Deleted temp working directory $MYTMPDIR" }
+
+# register the cleanup function to be called on the EXIT signal
+#trap cleanup EXIT
 
 if [[ $OS != 'Darwin' ]]; then
     LIB_PATH="--library=/usr/local/lib/R/site-library"
@@ -55,23 +64,46 @@ else
         exit 0
     fi
 
+    cd $MYTMPDIR
     PCK_PATH="$REP_LOC/$1"
     if [[ ! -d $PCK_PATH ]]; then
         printf "%s\n" "Could not find package source directory in $REP_LOC"
-        exit 1
-    else
 
-        PCK_NEW_VERSION=`grep -i version  $PCK_PATH/DESCRIPTION | grep -Eo [[:digit:]]\(.[[:digit:]]\)+`
-        BUILD_FILE="$PCK_NAME"'_'"$PCK_NEW_VERSION.tar.gz"
-        BUILD_FILE_PATH="$REP_LOC/$BUILD_FILE"
-
-        sudo R CMD REMOVE $PCK_NAME
-        sudo R CMD build $PCK_PATH
-        sudo R CMD INSTALL $LIB_PATH $BUILD_FILE
-        mv "`pwd`/$BUILD_FILE" "$HOME/build-packages/$BUILD_FILE"
-
-        if [[ $keep -ne 1 ]]; then
-            rm -vf $BUILD_FILE
+        if [[ -n "$REP_ALT_LOC" ]]; then
+            printf "%s\n" "Attempting using alternative path $REP_ALT_LOC"
+            PCK_PATH="$REP_ALT_LOC/$1"
+            if [[ ! -d $PCK_PATH ]]; then
+                printf "%s\n" "Could not find package in alternative source directory"
+                exit 1
+            fi
+        else    # no luck in alternative loc.
+            printf "%s\n" 'No alternative path found in "$REP_ALT_LOC" environment variable, exiting...'
+            exit 1
         fi
     fi
+
+    printf "%s\n" "$1 R package found in $PCK_PATH"
+
+    PCK_NEW_VERSION=`grep -i version $PCK_PATH/DESCRIPTION | egrep -o "[[:digit:]](.?[[:digit:]])+"`
+    BUILD_FILE="$PCK_NAME"'_'"$PCK_NEW_VERSION.tar.gz"
+    BUILD_FILE_PATH="$MYTMPDIR/$BUILD_FILE"
+    
+    printf "%s\n" "Running R install commands through sudo..."
+    sudo R CMD REMOVE $PCK_NAME
+    sudo R CMD build $PCK_PATH
+    sudo R CMD INSTALL -l $LIB_PATH "./$BUILD_FILE"
+
+    if [[ $keep -ne 1 ]]; then
+        rm -vf $BUILD_FILE
+    else
+        if [[ ! -d $HOME/build-packages ]]; then
+            printf "%s\n" "'build-packages' folder does not exist... creating it"
+            mkdir -p $HOME/build-packages
+        fi
+        mv "$BUILD_FILE_PATH" "$HOME/build-packages/$BUILD_FILE"
+        printf "%s\n" "Backup copy of installed package kept in $BUILD_PCKGS_DIR"
+    fi
+    cd $HOME
+    rm -rf $MYTMPDIR
 fi
+
